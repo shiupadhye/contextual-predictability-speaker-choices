3cc6c09a5b50cf99a66ec251ad8e9fe92d0f7350
Shivaa Upadhye <shiupadhye@github.com>
2025-10-07 18:36:14 -0700

    updated study 1 lexical category analysis script

--- Study1-probabilistic-reduction/lexicalCategory_analysis.R
+++ Study1-probabilistic-reduction/lexicalCategory_analysis.R
@@ -14,6 +14,7 @@ library(dominanceanalysis)
 
 scores <- read_csv('SWBD_DurAnalysisData.csv')
 scores <- scores %>% filter(Class != 'OTHER')
+scores$Class <- factor(scores$Class, levels = c('Function','Content'))
 
 # define backward predictability variants
 scores$delta_bwPred <- scores$infillBw_logProb - scores$infillFw_logProb
@@ -32,19 +33,28 @@ scores %>%
   group_by(lexical_category) %>%
   count()
 
+
 ##  main effect of func-content words
 m.FuncContentDist.mainEffect.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + delta_bwPred + Class + (1|speaker_id), data = scores)
 summary(m.FuncContentDist.mainEffect.deltaBwPred)
-
-m.FuncContentDist.mainEffect.condPMI <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + delta_bwPred + Class + (1|speaker_id), data = scores)
-summary(m.FuncContentDist.mainEffect.condPMI)
 
 ## interaction
 m.FuncContentDist.int.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb * Class + infillFw_logProb * Class + delta_bwPred * Class + (1|speaker_id), data = scores)
 summary(m.FuncContentDist.int.deltaBwPred)
 
+## get simple means
+emtrends(m.FuncContentDist.int.deltaBwPred, specs = "Class", var = "unigram_logProb",infer = c(TRUE, TRUE))
+
+emtrends(m.FuncContentDist.int.deltaBwPred, specs = "Class", var = "infillFw_logProb",infer = c(TRUE, TRUE))
+
+emtrends(m.FuncContentDist.int.deltaBwPred, specs = "Class", var = "delta_bwPred",infer = c(TRUE, TRUE))
+
+
 m.FuncContentDist.int.condPMI <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb * Class + infillFw_logProb * Class + condPMI * Class + (1|speaker_id), data = scores)
 summary(m.FuncContentDist.int.condPMI)
+
+
+
 
 ### visualize
 predict_interaction_with_ci <- function(model, x_var, group_var, n_points = 100, 
@@ -124,12 +134,18 @@ fwPred_class <- predict_interaction_with_ci(
 
 deltaBwPred_class <- predict_interaction_with_ci(
   model = m.FuncContentDist.mainEffect.deltaBwPred ,
-  x_var = "deltaBwPred",
+  x_var = "delta_bwPred",
+  group_var = "Class"
+)
+
+condPMI_class <- predict_interaction_with_ci(
+  model = m.FuncContentDist.mainEffect.condPMI ,
+  x_var = "condPMI",
   group_var = "Class"
 )
 
 ## visualizations
-ggplot(unigramPred_class, aes(x = unigram_logProb, y = predicted, color = class, fill = class)) +
+p1 <- ggplot(unigramPred_class, aes(x = unigram_logProb, y = predicted, color = Class, fill = Class)) +
   geom_line() +
   geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
   labs(x = expression(bold("Log Unigram Predictability")), y = "") +
@@ -141,3 +157,145 @@ ggplot(unigramPred_class, aes(x = unigram_logProb, y = predicted, color = class,
         axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
         plot.title = element_text(face = "bold", color = "black"),
         legend.title = element_text(face = "bold"))
+
+
+p2 <- ggplot(fwPred_class, aes(x = infillFw_logProb, y = predicted, color = Class, fill = Class)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Log Forward Predictability")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+p3 <- ggplot(deltaBwPred_class, aes(x = delta_bwPred, y = predicted, color = Class, fill = Class)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Delta Backward Predictability")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+p4 <- ggplot(condPMI_class, aes(x = condPMI, y = predicted, color = Class, fill = Class)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Conditional PMI")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+# save plots
+saveRDS(p1,'plots/rds/interaction_plot_freq.png')
+saveRDS(p2,'plots/rds/interaction_plot_fwPred.png')
+saveRDS(p3,'plots/rds/interaction_plot_bwPred.png')
+saveRDS(p4,'plots/rds/interaction_plot_PMI.png')
+
+# Replication of Bell et al. 2009
+scores.HF_func <- scores %>%
+  filter(lexical_category == 'High-Freq-Function')
+
+scores.LF_func <- scores %>%
+  filter(lexical_category == 'Mid-to-Low-Freq-Function')
+
+scores.content <- scores %>%
+  filter(lexical_category == 'Content')
+
+m.BellAnalysisRep.HF_func.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + delta_bwPred + (1|speaker_id), data = scores.HF_func)
+summary(m.BellAnalysisRep.HF_func.deltaBwPred)
+coef(summary(m.BellAnalysisRep.HF_func.deltaBwPred))
+
+m.BellAnalysisRep.HF_func.condPMI <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + condPMI + (1|speaker_id), data = scores.HF_func)
+summary(m.BellAnalysisRep.HF_func.condPMI)
+
+m.BellAnalysisRep.LF_func.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + delta_bwPred + (1|speaker_id), data = scores.LF_func)
+summary(m.BellAnalysisRep.LF_func.deltaBwPred)
+
+m.BellAnalysisRep.LF_func.condPMI <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + condPMI + (1|speaker_id), data = scores.LF_func)
+summary(m.BellAnalysisRep.LF_func.condPMI)
+
+m.BellAnalysisRep.content.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + delta_bwPred + (1|speaker_id), data = scores.content)
+summary(m.BellAnalysisRep.content.deltaBwPred)
+
+m.BellAnalysisRep.content.condPMI <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb + infillFw_logProb + condPMI + (1|speaker_id), data = scores.content)
+summary(m.BellAnalysisRep.content.condPMI)
+
+
+m.lexCat.int.deltaBwPred <- lmer(duration * 1000 ~ word_len + uttrSR + age + sex + unigram_logProb * lexical_category + infillFw_logProb * lexical_category + delta_bwPred * lexical_category + (1|speaker_id), data = scores)
+summary(m.lexCat.int.deltaBwPred)
+
+unigramPred_lexCat <- predict_interaction_with_ci(
+  model =m.lexCat.int.deltaBwPred ,
+  x_var = "unigram_logProb",
+  group_var = "lexical_category"
+)
+
+ggplot(unigramPred_lexCat, aes(x = unigram_logProb, y = predicted, color = lexical_category, fill = lexical_category)) +
+  geom_line() +
+  geom_point() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Log Unigram Predictability")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+fwPred_lexCat <- predict_interaction_with_ci(
+  model =m.lexCat.int.deltaBwPred ,
+  x_var = "infillFw_logProb",
+  group_var = "lexical_category"
+)
+
+ggplot(fwPred_lexCat, aes(x = infillFw_logProb, y = predicted, color = lexical_category, fill = lexical_category)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Log Forward Predictability")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+
+bwPred_lexCat <- predict_interaction_with_ci(
+  model =m.lexCat.int.deltaBwPred ,
+  x_var = "delta_bwPred",
+  group_var = "lexical_category"
+)
+
+ggplot(bwPred_lexCat, aes(x = delta_bwPred, y = predicted, color = lexical_category, fill = lexical_category)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = ci_low, ymax = ci_high), alpha = 0.4, color = NA) +
+  labs(x = expression(bold("Delta BwPred")), y = "") +
+  theme_minimal() +
+  #theme(legend.position = "none")  +
+  theme(axis.text.x = element_text(face = "bold", color = "black"),  # x-axis text
+        axis.text.y = element_text(face = "bold", color = "black"),  # y-axis text
+        axis.title.x = element_text(face = "bold", color = "black"),  # x-axis title
+        axis.title.y = element_text(face = "bold", color = "black"),  # y-axis title
+        plot.title = element_text(face = "bold", color = "black"),
+        legend.title = element_text(face = "bold"))
+
+
+
+  
